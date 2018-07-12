56bb7c72ac583245c94e9f87d299cf3cc53f9c99
Osetrova <liberty_20_07@mail.ru>
2018-07-12 08:50:07 +0300

    lesson2

--- src/lesson2/Task1.java
+++ src/lesson2/Task1.java
@@ -22,61 +22,61 @@ public class Task1 {
 
     public static void formatDate(int day, int month, int year) {
 
-            switch (month) {
-                case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                case 12:
-                    if (day >= 1 && day < 31) {
-                        day++;
-                    } else if (day == 31) {
-                        day = 1;
-                        month++;
-                    }else {
-                        System.out.println("Дата неверная");
-                    }
-                    break;
-                case 2:
-                    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-                        if (day >= 1 && day < 29) {
-                            day++;
-                        } else if (day == 29) {
-                            day = 1;
-                            month++;
-                        }else {
-                            System.out.println("Дата неверная");
-                        }
-                    } else {
-                        if (day >= 1 && day < 28) {
-                            day++;
-                        } else if (day == 28) {
-                            day = 1;
-                            month++;
-                        }else {
-                            System.out.println("Дата неверная");
-                        }
-                    }
-                    break;
-                case 4:
-                case 6:
-                case 9:
-                case 11:
-                    if (day >= 1 && day < 30) {
-                        day++;
-                    } else if (day == 30) {
-                        day = 1;
-                        month++;
-                    }else {
-                        System.out.println("Дата неверная");
-                    }
-                    break;
-                default:
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                if (day >= 1 && day < 31) {
+                    day++;
+                } else if (day == 31) {
+                    day = 1;
+                    month++;
+                } else {
                     System.out.println("Дата неверная");
+                }
+                break;
+            case 2:
+                if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+                    if (day >= 1 && day < 29) {
+                        day++;
+                    } else if (day == 29) {
+                        day = 1;
+                        month++;
+                    } else {
+                        System.out.println("Дата неверная");
+                    }
+                } else {
+                    if (day >= 1 && day < 28) {
+                        day++;
+                    } else if (day == 28) {
+                        day = 1;
+                        month++;
+                    } else {
+                        System.out.println("Дата неверная");
+                    }
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (day >= 1 && day < 30) {
+                    day++;
+                } else if (day == 30) {
+                    day = 1;
+                    month++;
+                } else {
+                    System.out.println("Дата неверная");
+                }
+                break;
+            default:
+                System.out.println("Дата неверная");
 
-            }
+        }
         System.out.println(day + "." + month + "." + year);
     }
 }
--- src/lesson2/Task3.java
+++ src/lesson2/Task3.java
@@ -6,40 +6,38 @@ public class Task3 {
 
     public static void main(String[] args) {
         Scanner n1 = new Scanner(System.in);
-        System.out.println("Квадратное уравнение вида ax ² + bx + c = 0.\nВведите а:" );
+        System.out.println("Квадратное уравнение вида ax ² + bx + c = 0.\nВведите а:");
         int a = n1.nextInt();
 
-        System.out.println("Введите b:" );
+        System.out.println("Введите b:");
         int b = n1.nextInt();
 
-        System.out.println("Введите c:" );
+        System.out.println("Введите c:");
         int c = n1.nextInt();
 
         double discriminant = discriminant(a, b, c);
 
-        if (isPositive(discriminant)){
-            double x1 = (-b+Math.sqrt(discriminant))/(2*a);
-            double x2 = (-b-Math.sqrt(discriminant))/(2*a);
+        if (isPositive(discriminant)) {
+            double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
             System.out.println("Дискриминант больше нуля. Корни уравнения: " + x1 + ", " + x2);
-        }
-        else if (isZero(discriminant)){
-            double x1 = (-b)/(2.0*a);
+        } else if (isZero(discriminant)) {
+            double x1 = (-b) / (2.0 * a);
             System.out.println("Дискриминант равен нулю. Корень уравнения: " + x1);
-        }
-        else if (!isPositive(discriminant)){
+        } else {
             System.out.println("Отрицательный дискриминант. Нет корней.");
         }
     }
 
-    public static double discriminant(int a, int b, int c){
-        return b*b-4.0*a*c;
+    public static double discriminant(int a, int b, int c) {
+        return b * b - 4.0 * a * c;
     }
 
-    public static boolean isPositive(double discriminant){
+    public static boolean isPositive(double discriminant) {
         return discriminant > 0;
     }
 
-    public static boolean isZero(double discriminant){
+    public static boolean isZero(double discriminant) {
         return discriminant == 0;
     }
 }
